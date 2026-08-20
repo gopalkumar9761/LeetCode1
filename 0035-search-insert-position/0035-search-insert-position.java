032518ea36089410686381fb28e440ec29af3351
Gopal kumar <gopalkumar550550@gmail.com>
2026-08-20 19:00:43 +0530

    Time: 0 ms (100%), Space: 44.4 MB (96.65%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int first = 0;
+        int last = nums.length;
+
+        while (first < last) {
+            int mid = first + (last - first) / 2;
+            if(nums[mid] >= target) {
+                last = mid;
+            }
+            else {
+                first = mid + 1;
+            }
+        }
+        return first;
+    }
+}
